4d33e55bed2b4a383bc2bc81072400ddc506e217
Lornzo <a6288678@hotmail.com>
2020-07-07 14:39:26 +0800

    新增進階操作資料庫物件sqli_db_opration.class.php

---
+++ sqli_db_opration.class.php
@@ -0,0 +1,42 @@
+<?php
+if(!class_exists("sqli_db_switch")){require(__DIR__."sqli_db_switch.class.php");}
+
+
+/**
+ * 繼承自sqli_db_switch開發的MySQL操作物件，內含比較危險的操作Function，請小心使用
+ * @requires sqli_db_switch 父類別物件
+ * @author Lornzo Lee(李赤兔) <a6288678@hotmail.com>
+ * @version 2020.7.7
+ */
+class sqli_db_opration extends sqli_db_switch{
+    
+    /**
+     * 清空整張資料表
+     * @return bool
+     */
+    public function truncateTable(){
+        return !empty($this->_table)?$this->doQuery("TRUNCATE ".$this->_table.";"):false;
+    }
+    
+    /**
+     * 刪除整張資料表
+     * @return bool
+     */
+    public function dropTable(){
+        return !empty($this->_table)?$this->doQuery("DROP TABLE IF EXISTS ".$this->_table.";"):false;
+    }
+    
+    
+    public function showDatabases(){
+        
+        
+    }
+    
+    public function showTables(){
+        $db_tables = mysqli_fetch_arr($this->doQuery("SHOW TABLES;"),MYSQLI_ASSOC);
+        $result = array();
+
+
+    }
+}
+?>
