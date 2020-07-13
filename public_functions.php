aea433274905d16e247f51f50449b92da7393f49
Lornzo <a6288678@hotmail.com>
2020-07-13 16:06:01 +0800

    update

--- public_functions.php
+++ public_functions.php
@@ -3,4 +3,12 @@
  * 這裡有一些常用的，不必將之物件化的Function
  */
 
+/**
+ * 過濾要進資料庫的字串
+ * @param string $str
+ * @return string
+ */
+function dataFilter(string $str){
+    return trim(stripslashes(addslashes(htmlspecialchars($str, ENT_QUOTES, 'UTF-8'))));
+}
 ?>
