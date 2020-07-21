e748d246958eca6c7886eba177a7080b079581c6
Lornzo <a6288678@hotmail.com>
2020-07-22 02:43:36 +0800

    新增wordpress資料庫讀取用的物件-未完成

---
+++ wp_db_switch.class.php
@@ -0,0 +1,12 @@
+<?php
+if(!class_exists("sqli_db_switch")){require(__DIR__."/sqli_db_switch.class.php");}
+/**
+ * 繼承自sqli_db_switch開發的MySQL操作物件，主要是用於wordpress的資庫讀取
+ * @requires sqli_db_switch.class.php 父類別物件
+ * @author Lornzo Lee(李赤兔) <a6288678@hotmail.com>
+ * @version 2020.7.22
+ */
+class wp_db_switch extends sqli_db_switch{
+    
+}
+?>
