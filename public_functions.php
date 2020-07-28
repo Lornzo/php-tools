8101124f995ee63229653f65ce1394d28a6fefed
Lornzo <a6288678@hotmail.com>
2020-07-28 10:43:39 +0800

    新增發送header功能

--- public_functions.php
+++ public_functions.php
@@ -11,4 +11,19 @@
 function dataFilter(string $str){
     return trim(stripslashes(addslashes(htmlspecialchars($str, ENT_QUOTES, 'UTF-8'))));
 }
+
+/**
+ * 根壉代號來發送http code
+ * @param string $code
+ */
+function setHeaderByCode(string $code){
+    $header_str = $code;
+    switch($code){
+        case "404":
+            $header_str = "HTTP/1.1 404 Not Found";
+            break;
+    }
+    header($header_str);
+    exit();
+}
 ?>
