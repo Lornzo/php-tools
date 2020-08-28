ebe2e9dacc3d5324874673823640e1f2f171e038
Lornzo <a6288678@hotmail.com>
2020-08-29 04:09:20 +0800

    新增msggo

--- public_functions.js.php
+++ public_functions.js.php
@@ -13,4 +13,17 @@ function errback($msg){
     echo "history.go(-1);";
     echo "</script>";
 }
+
+/**
+* 送出訊息後，轉跳到指定的位置
+* @param type $msg
+* @param type $loc
+* @param type $target
+*/
+function msggo($msg,$loc, $target = "self"){
+       echo "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"><script>";
+       echo "alert('$msg');";
+       echo "".$target.".location.href='$loc';";
+       echo "</script>";
+}
 ?>
